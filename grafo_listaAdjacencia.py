51e7ac46ca84b396481e6190608d676b45ddc1ff
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-03-21 18:20:47 -0300

    Update grafo_listaAdjacencia.py

--- grafo_listaAdjacencia.py
+++ grafo_listaAdjacencia.py
@@ -1,111 +1,128 @@
 from collections import defaultdict
 
+"""Alexandre, Diferentemente da matriz de adjacências, na lista de adjacências
+não é preciso passar a quantidade de vértices para a instanciação do grafo.
+Sugiro que remova do construtor da classe o parâmetro "numeroVertices".
+Assim, é possível criar um grafo vazio G=Grafo() e ir adicionando vértices
+e arestas de maneira incremental e sem a limitação da quantidade de
+vértices inicial. No seu código atual, ao fazer G=Grafo(5) e não adicionar
+nenhum vértice, o grafo terá ordem 5, o que está incorreto já que ele não
+possui nenhum vértice. Também sugiro que você reveja o seu
+método imprime_lista_adjacencias, pois a sua impressão está replicando a lista.
+No exemplo apresentado no vídeo, somente as 3 últimas linhas deveriam ter sido
+apresentadas."""
+
+# u = vertice inicial/origem
+# v = vertice final/destino
+
+# criacao constructor da classe Grafos
 class Grafo:
-  # construtor da classe com atributos numero_vertices, numero_arestas e lista adjacente
-  def __init__(self, numeroVertices):
-    self.numero_vertices = numeroVertices
-    self.numero_arestas = 0
-    self.adjacenteLista = defaultdict(list)
+  def __init__(self):
+    self.adjacente_list = defaultdict(list)
 
   """def __str__(self):
     return self.imprime_lista_adjacencias()"""
 
  # ponto a
- def adiciona_vertice(self, u_vertice_inicial):
-    if u_vertice_inicial not in self.adjacenteLista:
-      self.adjacenteLista[u_vertice_inicial] = []
-      self.numero_vertices += 1
+ # funcao adciona vertice, faz o teste da existencia do mesmo,
+ # cria a vertice
+  def adiciona_vertice(self, u):
+    if u not in self.adjacente_list:
+      self.adjacente_list[u] = []
 
   # ponto b
-  def adiciona_aresta(self, u_vertice_inicial, v_vertice_final, peso):
-    if u_vertice_inicial not in self.adjacenteLista:
-      self.adjacenteLista[u_vertice_inicial]
-    if v_vertice_final not in self.adjacenteLista:
-      self.adjacenteLista[v_vertice_final]
+  def adiciona_aresta(self, u, v, peso):
+    # verifica a existencias de ambos vertices
+    self.adiciona_vertice(u)
+    self.adiciona_vertice(v)
 
-    self.adjacenteLista[u_vertice_inicial].append((v_vertice_final, peso))
-    self.numero_arestas += 1
-    
-  
+    # adciona aresta e pondera a mesma
+    self.adjacente_list[u].append((v, peso))
+
   # ponto c
-  def remove_aresta(self, u_vertice_inicial, v_vertice_final):
-      if u_vertice_inicial in self.adjacenteLista:
-        for i, (destino, peso) in enumerate(self.adjacenteLista[u_vertice_inicial]):
-          if destino == v_vertice_final:
-            del self.adjacenteLista[u_vertice_inicial][i]
-            self.numero_arestas -= 1
-            print(f"Aresta de {u_vertice_inicial} para {v_vertice_final} removida com sucesso.")
-            return
-      print(f"Aresta de {u_vertice_inicial} para {v_vertice_final} não encontrada.")
-      
-  
+  # remocao da aresta do vertice U para vertice V
+  def remove_aresta(self, u, v):
+    for i in range(len(self.adjacente_list[u])):
+      if self.adjacente_list[u][i][0] == v:
+        del self.adjacente_list[u][i]
+        break
+
   # ponto d
-def remove_vertice(self, u_vertice_inicial):
-      if u_vertice_inicial not in self.adjacenteLista:
-        print(f"Vertice {u_vertice_inicial} não se encontra.")
-        return
+  # retira todas as arestas que se direcionam para o vertice U
+  def remove_vertice(self, u):
+    for vertice, arestas in self.adjacente_list.items():
+      arestas_verificadas = []
+      for destino, peso in arestas:
+        if destino != u:
+          arestas_verificadas.append((destino, peso))
+      self.adjacente_list[vertice] = arestas_verificadas
 
-      removidaAresta = len(self.adjacenteLista[u_vertice_inicial])
-      del self.adjacenteLista[u_vertice_inicial]
-
-      for v_vertice_final in list(self.adjacenteLista):##itera sobre o restante dos vertices
-        removidaAresta += sum(1 for destino, _ in self.adjacenteLista[v_vertice_final] if destino == u_vertice_inicial)
-        self.adjacenteLista[v_vertice_final] = [(destino, peso) for destino, peso in self.adjacenteLista[v_vertice_final] if destino != u_vertice_inicial]
-      
-      self.numero_vertices -= 1
-      self.numero_arestas -= removidaAresta
-      print(f"Vertice {u_vertice_inicial} e suas arestas foram removidas")
+      if u in self.adjacente_list:
+        del self.adjacente_list[u]
 
   # ponto e
- def tem_aresta(self, u_vertice_inicial, v_vertice_final):
-    if u_vertice_inicial in self.adjacenteLista:
-      for destino, _ in self.adjacenteLista[u_vertice_inicial]:
-        if destino == v_vertice_final:
-          return True
+  # faz a verificacao de arestas entre vertice U para vertice V
+  def tem_aresta(self, u, v):
+    for destino, _ in self.adjacente_list[u]:
+      if destino == v:
+        return True
     return False
 
   # ponto f
-  def grau_entrada(self, u_vertice_inicial):
-    grau_entrada = 0
-    for i in self.adjacenteLista:
-      for destino, _ in self.adjacenteLista[i]:
-        if destino == u_vertice_inicial:
-          grau_entrada += 1
-    return grau_entrada
+  def grau_entrada(self, u):
+    count = 0
+    # percorre todos vertices
+    for vertice in self.adjacente_list:
+      # percorre cada aresta do vertice
+      for destino, _ in self.adjacente_list[vertice]:
+        if destino == u:
+          count += 1
+    return count
 
   # ponto g
- def grau_saida(self, u_vertice_inicial):
-    if u_vertice_inicial in self.adjacenteLista:
-      return len(self.adjacenteLista[u_vertice_inicial])
-    return 0
+  def grau_saida(self, u):
+    # grau de saida de U e o tamanho da lsita de adjancencias / GRAFO
+    return len(self.adjacente_list[u])
 
   # ponto h
- def grau(self, u_vertice_inicial):
-    return self.grau_entrada(u_vertice_inicial) + self.grau_saida(u_vertice_inicial)
+  def grau(self, u):
+    grau_saida = len(self.adjacente_list[u])
+    
+    # contador do grau de entrada
+    grau_ent = 0
+    for vertice in self.adjacente_list:
+      for destino, _ in self.adjacente_list[vertice]:
+        if destino == u:
+          grau_ent += 1
+    # grau total e encontrado na somatoria dos graus de saida e entrada
+    return grau_ent + grau_saida
 
   # ponto i
- def get_peso(self, u_vertice_inicial, v_vertice_final):
-    if u_vertice_inicial not in self.adjacenteLista:
-      print(f"Vertice {u_vertice_inicial} não existente")
-      return None
-    for destino, peso in self.adjacenteLista[u_vertice_inicial]:
-      if destino == v_vertice_final:
+  def get_peso(self, u, v):
+    # percorre toda lista do vertice U
+    for destino, peso in self.adjacente_list[u]:
+      # verifica se o destino da aresta e igual ao vertice V
+      if destino == v:
         return peso
-
-    print(f"Aresta de {u_vertice_inicial} para {v_vertice_final} não encontrada")
+        # retorna o peso da aresta e caso nao encotre retorna None (excecao)
     return None
+    
 
   # pont j
   def imprime_lista_adjacencias(self):
-    impressaoTotal = ""
-    for vertice in self.adjacenteLista:
-      impressao = f"{vertice}:"
-      for destino, peso in self.adjacenteLista[vertice]:
-        impressao += f"('{destino}', {peso}) ->"
-      impressaoTotal += impressao.strip(", ") + "\n"
-    print(impressaoTotal)
-        
-g = Grafo(4)
+    # percorre cada vertice
+    for vertice, arestas in self.adjacente_list.items():
+      # informa a linha de impressao e nome do vertice
+      line = f"{vertice}"
+      # percorre cada aresta do vertice atual
+      for destino, peso in arestas:
+        # adciona a representacao da aresta a linha
+        line += f"('{destino}', {peso}) -> "
+      
+      line = line.rstrip(" -> ") # remove o ultimo " -> "
+      print(line)
+
+g = Grafo()
 
 """g.adjacenteLista["Pedro"].append(("Maria",3))
 g.adjacenteLista['Maria'].append(('Clara', 2))
